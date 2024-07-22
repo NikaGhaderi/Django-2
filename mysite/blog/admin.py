44c78c776e87cebeaf1720b0751893a8a87e341d
NikaGhaderi <nika.ghadery@gmail.com>
2024-07-23 01:30:23 +0330

    Admin page edited

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created')
+    list_filter = ('title', 'status', 'created')
+    search_fields = ('title', 'body')
+
+
+admin.site.register(Post, PostAdmin)
